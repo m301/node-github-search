2dccc538a747fcd2bdc243dce55a0e45ff322827
Madhurendra Sachan <themadhurendra@gmail.com>
2017-09-24 17:28:26 +0530

    console logging.

--- server.js
+++ server.js
@@ -37,5 +37,6 @@ app.use(function(err, req, res, next) {
     });
 });
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 8080
 app.listen(port);
+console.log("Listening on port "+port);
