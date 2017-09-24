8b6b3969c0f93f446ed18020e00e012bb28f23a1
Madhurendra Sachan <themadhurendra@gmail.com>
2017-09-24 18:42:46 +0530

    added readme and updated routes.

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,20 @@ router.get('/', function (req, res) {
     res.render('index', {title: 'Github User Search'});
 });
 
+
+var CACHE = {};
 //this can be moved to another file.
 router.get("/search", function (req, res) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
+
+    if (CACHE[query.query])
+        return res.send(CACHE[query.query]);
     request.get({
         url: "https://api.github.com/users/" + query.query,
-        headers: {Accept: " application/vnd.github.preview","User-Agent":"test-ws"}
+        headers: {Accept: " application/vnd.github.preview", "User-Agent": "test-ws"}
     }, function (error, response, body) {
-        res.send(JSON.parse(body))
+        res.send((CACHE[query.query] = JSON.parse(body)));
     });
 });
 module.exports = router;
